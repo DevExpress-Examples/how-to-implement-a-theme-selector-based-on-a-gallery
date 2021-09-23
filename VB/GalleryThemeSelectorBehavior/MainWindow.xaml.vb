f82a05a755ca461eb632e8fabb9f076d25ca4708
DevExpressExampleBot <fakeEmail>
2021-09-23 18:32:02 +0300

    Source auto update [skip ci]

--- VB/GalleryThemeSelectorBehavior/MainWindow.xaml.vb
+++ VB/GalleryThemeSelectorBehavior/MainWindow.xaml.vb
@@ -1,4 +1,4 @@
-﻿Imports System
+Imports System
 Imports System.Collections.Generic
 Imports System.Linq
 Imports System.Text
@@ -14,14 +14,15 @@ Imports System.Windows.Navigation
 Imports System.Windows.Shapes
 
 Namespace GalleryThemeSelectorBehavior
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
 End Namespace
